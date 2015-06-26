2704523ca3f68d31617f90ac2395e72a310760ee
Chonhan <chonhan@gmail.com>
2015-06-26 23:00:54 +0800

    almost finish

--- src/idv/chonhan/Wedding919Servlet.java
+++ src/idv/chonhan/Wedding919Servlet.java
@@ -1,13 +1,18 @@
 package idv.chonhan;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class Wedding919Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+			throws IOException, ServletException {
+		RequestDispatcher view = req.getRequestDispatcher("register.html");
+		view.forward(req, resp);
 	}
 }
--- war/js/wedding919.js
+++ war/js/wedding919.js
@@ -1,22 +1,30 @@
 function getPhotoData() {
+    var imageBase = "https://dl.dropboxusercontent.com/u/9780124/wedding919/images/";
+    var thumbBase = "https://dl.dropboxusercontent.com/u/9780124/wedding919/thumbs/";
     var photos = [
         {
-            date: '2015-06-26',
-            file: 'img/avatar01.png',
-            thumb: 'img/avatar01.png',
+            date: '2015-06-23',
+            file: imageBase + '8P4A4547.jpg',
+            thumb: thumbBase + '8P4A4547.jpg',
             title: '系列第一張'
         },
         {
-            date: '2015-06-27',
-            file: 'img/avatar02.png',
-            thumb: 'img/avatar01.png',
+            date: '2015-06-24',
+            file: imageBase + '8P4A4556.jpg',
+            thumb: thumbBase + '8P4A4556.jpg',
             title: '系列第二張'
         },
         {
-            date: '2015-06-28',
-            file: 'img/map.png',
-            thumb: 'img/avatar01.png',
+            date: '2015-06-25',
+            file: imageBase + '8P4A4558.jpg',
+            thumb: thumbBase + '8P4A4558.jpg',
             title: '系列第三張'
+        },
+        {
+            date: '2015-06-26',
+            file: imageBase + '8P4A4568.jpg',
+            thumb: thumbBase + '8P4A4568.jpg',
+            title: '系列第四張'
         },
     ];
 
